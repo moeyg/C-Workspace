94191c290ef6288496fffea76070eb32b1fbb352
moeyg <moooeyg@gmail.com>
2022-08-23 17:42:00 +0900

    Add : 02.syntax_error.c

---
+++ Note/03.function/02.syntax_errors.c
@@ -0,0 +1,14 @@
+// Syntax Error (문법 오류)
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y, z;
+
+    // error: use of undeclared identifier 'num'
+    // 'num' 변수가 선언되지 않은 syntax error
+    num = 42;
+
+    return 0;
+}
