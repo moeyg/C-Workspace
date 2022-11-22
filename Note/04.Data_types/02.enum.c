e17285aebae6b76fe3ac793c971f6b4cd867fe93
moeyg <moooeyg@gmail.com>
2022-11-22 19:02:27 +0900

    Add : 02.enum.c

---
+++ Note/04.Data_types/02.enum.c
@@ -0,0 +1,42 @@
+// enum
+// enumeration : 열거형
+// int와 섞어 사용 가능하기 때문에 별명을 붙이는 수준으로 볼 수 있다.
+// int형 값만 저장이 가능
+
+#include <stdio.h>
+
+int main(void)
+{
+    enum day
+    {
+        DAY_MONDAY,
+        DAY_TUESDAY,
+        DAY_WEDNESDAY,
+        DAY_TURSDAY,
+        DAY_FRIDAY,
+        DAY_SATURDAY,
+        DAY_SUNDAY,
+    };
+
+    enum month
+    {
+        MONTH_JAN,
+        MONTH_FEB,
+        MONTH_MAR,
+        MONTH_APR,
+        MONTH_MAY,
+        MONTH_JUN,
+        MONTH_JUL,
+        MONTH_AUG,
+        MONTH_SEP,
+        MONTH_OCT,
+        MONTH_NOV,
+        MONTH_DEC,
+    };
+
+    enum day what_day_is_today = DAY_WEDNESDAY;
+    enum month what_month_is_now = MONTH_NOV;
+
+    printf("%d\n", what_day_is_today); // 2
+    printf("%d\n", what_month_is_now); // 10
+}
