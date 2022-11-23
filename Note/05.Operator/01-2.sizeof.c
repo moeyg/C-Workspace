7945c43d3e1d6f0773c34d2c953786645efdb84d
moeyg <moooeyg@gmail.com>
2022-11-23 10:25:00 +0900

    Add : 01-2.sizeof.c

---
+++ Note/05.Operator/01-2.sizeof.c
@@ -0,0 +1,19 @@
+// sizeof()
+// 배열의 경우, 함수 인자로 받을 경우 다른 결과가 나온다.
+
+#include <stdio.h>
+
+size_t get_char_array_size(char data[])
+{
+    return sizeof(*data);
+}
+
+int main(void)
+{
+    char char_array[42];
+
+    size_t size_array = sizeof(char_array); // 42
+    size_t size_array_by_func = get_char_array_size(char_array); // 4
+
+    return 0;
+}
