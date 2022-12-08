aa46e87591dc9949e61ffb6d5222b6f56d619e80
moeyg <moooeyg@gmail.com>
2022-12-08 16:15:40 +0900

    Add : 15.const_pointer.c

---
+++ Note/08.Pointer/15.const_pointer.c
@@ -0,0 +1,26 @@
+// Const in p
+
+int main(void)
+{
+    char ch = 'A';
+    int num1 = 10;
+    int num2 = 20;
+
+    // 주소를 보호하는 const
+    // p is a const to int
+    int* const p = &num1;
+    // p = &num2; // 컴파일 오류 -> 주소 변경 X
+    // ++p; // 컴파일 오류 -> 주소 변경 X
+    int* pointer = p; // 값을 복사해서 변경은 가능하다.
+    *pointer = 42;
+
+    // 메모리 주소 안에 저장된 값을 보호하는 const
+    // ptr is a pointer to const int
+    // const int* ptr == int const* ptr
+    const int* ptr = &num1;
+    ptr = &num2; // 주소 변경 가능
+
+    // 주소와 값을 보호하는 const
+    // c is a const pointer to const char
+    const char* const c = &ch;
+}
