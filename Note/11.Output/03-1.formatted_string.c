b633d18841475ae4d9937eab31a2c0363fd9e819
moeyg <moooeyg@gmail.com>
2022-12-18 22:12:59 +0900

    Add : 03.formatted string.c

---
+++ Note/11.Output/03-1.formatted_string.c
@@ -0,0 +1,12 @@
+// Formatted string
+// 일반적인 서식 문자열 형식
+// 일반적으로 % 뒤에 최대 4개의 지정자를 가질 수 있다.
+// %[flag] [width] [.numeric precision | .string min/max output count] [length] format specifier
+
+// 1. flag (플래그) - 선택
+// 2. width (너비) - 선택
+// 3. precision (정밀도) - 선택
+// 3-1. .numeric precision (숫자 정밀도)
+// 3-2. .string min/max output count (문자열 최소/최대 출력 개수)
+// 4. length (길이) - 선택
+// 5. format specifier (서식 지정자) - ✨ 필수
