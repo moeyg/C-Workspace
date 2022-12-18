e92bfcb4378e36422f44613b1539300894fd69ac
moeyg <moooeyg@gmail.com>
2022-12-18 19:21:53 +0900

    Chore : Change function name

--- Note/10.string/02.strlen.c
+++ Note/10.string/02.strlen.c
@@ -2,18 +2,18 @@
 
 #include <stdio.h>
 
-size_t ft_strlen(char const *str);
+size_t strlen_func(char const *str);
 
 int main(void)
 {
     char str[] = "Hello world";
     
-    printf("str length is %lu\n", ft_strlen(str)); // str length is 11
+    printf("str length is %lu\n", strlen_func(str)); // str length is 11
 
     return 0;
 }
 
-size_t ft_strlen(char const *str)
+size_t strlen_func(char const *str)
 {
     size_t length = 0;
 
