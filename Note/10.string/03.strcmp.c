141e5552af2dde14406f1bdb4140bb9f8d34d5d8
moeyg <moooeyg@gmail.com>
2022-12-18 19:22:05 +0900

    Chore : Change function name

--- Note/10.string/03.strcmp.c
+++ Note/10.string/03.strcmp.c
@@ -5,20 +5,20 @@
 
 #include <stdio.h>
 
-int ft_strcmp(const char* str1, const char* str2);
+int strcmp_func(const char* str1, const char* str2);
 
 int main(void)
 {
     const char* str1 = "ABCDef";
     const char* str2 = "ABCDEf";
-    int result = ft_strcmp(str1, str2);
+    int result = strcmp_func(str1, str2);
 
     printf("%d\n", result); // 32 -> str1 > str2
 
     return 0;
 }
 
-int ft_strcmp(const char* str1, const char* str2)
+int strcmp_func(const char* str1, const char* str2)
 {
     while (*str1 != '\0' && (*str1 == *str2))
     {
